969f5f9a6b59265446e925705dfd7dd24a990157
Nicola Getsee <Nicola.getsee@gmail.com>
2024-10-06 18:00:27 +0000

    completed homepage header and css

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 import os
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 path('', include('home.urls')),
     path('admin/', admin.site.urls),
     path('accounts/',include('allauth.urls')), # the / makes sure that this is included in the all auths
-]
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
