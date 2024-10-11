c5d76fdbbb9a9436f01389af576aaa6f3d1167b1
Nicola Getsee <Nicola.getsee@gmail.com>
2024-10-11 18:12:05 +0000

    added Category filtering

--- products/views.py
+++ products/views.py
@@ -1,28 +1,37 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q # generates a search query
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
-
     products = Product.objects.all()
     query = None
+    categories = None
 
+#checking does this exsist
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            #split list at commas then filter it down to onoly products whose category name is in list, double __ is looking at a field ie name field
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
-            query = request.GET['q'] # is q in query
+            query = request.GET['q']
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
             
-            queries = Q(name__icontains=query) | Q(description__icontains=query) # either its in the product name or prod description this is what Q does it checks
-            products = products.filter(queries) # filter products
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
