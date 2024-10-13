ecbf7869b658013c143b05f05c92c03e820358f1
Nicola Getsee <Nicola.getsee@gmail.com>
2024-10-13 10:41:47 +0000

    added sorting and product count to products page

--- products/views.py
+++ products/views.py
@@ -1,39 +1,36 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
-from django.db.models import Q # generates a search query
+from django.db.models import Q
 from .models import Product, Category
 
 # Create your views here.
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
+
     products = Product.objects.all()
     query = None
     categories = None
     sort = None
     direction = None
-    
 
-
-#checking does this exsist
     if request.GET:
         if 'sort' in request.GET:
             sortkey = request.GET['sort']
             sort = sortkey
             if sortkey == 'name':
-                sortkey = 'lower_name' #set it to lowername
+                sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
-                    sortkey = f'-{sortkey}' #reverses order
+                    sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-
-
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
-            #split list at commas then filter it down to onoly products whose category name is in list, double __ is looking at a field ie name field
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
 
@@ -45,7 +42,7 @@ def all_products(request):
             
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
-    
+
     current_sorting = f'{sort}_{direction}'
 
     context = {
